0c763d9b3641a6b1c181309c639fc1fdf231dce9
Đăng Tú <6233777-ngdangtu@users.noreply.gitlab.com>
2024-02-29 17:10:08 +0700

    update core test
    - add namespace for test event
    - fix 1 failed test
    - add argument test
    - correct terms

--- test/core.test.ts
+++ test/core.test.ts
@@ -1,4 +1,4 @@
-import { assertEquals } from 'test'
+import { assert, assertEquals } from 'test'
 import PriorityEvent, { type Listener } from 'core'
 
 declare global {
@@ -8,17 +8,17 @@ declare global {
       'deno/unload': Event
       'deno/unhandledrejection': PromiseRejectionEvent
       'deno/rejectionhandled': PromiseRejectionEvent
-      'void-event': unknown
-      'filter-event': (a: number) => `#${string}`
-      'test-listener': unknown
+      'core/void-event': unknown
+      'core/filter-event': (a: number) => `#${string}`
+      'core/test-listener': (a: unknown) => void
    }
 }
 
 function f404() {}
 
-Deno.test('[class] PriorityEvent', async (t) => {
-   const e = PriorityEvent.init()
+const e = PriorityEvent.init()
 
+Deno.test('[class] PriorityEvent', async (t) => {
    await t.step('must have only 1 instance!', () => {
       const newinstance = PriorityEvent.init()
       assertEquals<PriorityEvent>(e, newinstance)
@@ -31,9 +31,9 @@ Deno.test('[class] PriorityEvent', async (t) => {
       e.addListener('deno/unhandledrejection', f404)
       e.addListener('deno/rejectionhandled', f404)
 
-      const all = e.lsEvents()
-      assertEquals<number>(all.length, 5)
-      assertEquals<(keyof PriorityEventMap)[]>(all, [
+      const actual = e.lsEvents()
+      assertEquals<number>(actual.length, 5)
+      assertEquals<(keyof PriorityEventMap)[]>(actual, [
          'deno/load',
          'deno/beforeunload',
          'deno/unload',
@@ -52,17 +52,17 @@ Deno.test('[class] PriorityEvent', async (t) => {
       assertEquals<number>(e.lsEvents().length, 0)
    })
 
-   await t.step('add 5 listeners to "void-event" event with different priority', () => {
-      e.addListener('void-event', f404, 9999)
-      e.addListener('void-event', f404, 0)
-      e.addListener('void-event', f404, 9)
-      e.addListener('void-event', f404, 11)
-      e.addListener('void-event', f404, 100)
+   await t.step('add 5 listeners to "core/void-event" event with different priority', () => {
+      e.addListener('core/void-event', f404, 9999)
+      e.addListener('core/void-event', f404, 0)
+      e.addListener('core/void-event', f404, 9)
+      e.addListener('core/void-event', f404, 11)
+      e.addListener('core/void-event', f404, 100)
 
-      const all = e.lsListeners('void-event')
-      assertEquals<number | undefined>(all?.length, 5)
+      const actual = e.lsListeners('core/void-event')
+      assertEquals<number | undefined>(actual?.length, 5)
       assertEquals<[number, Listener][] | null>(
-         all,
+         actual,
          [
             [9999, f404],
             [0, f404],
@@ -74,10 +74,10 @@ Deno.test('[class] PriorityEvent', async (t) => {
       )
    })
 
-   await t.step('listen "void-event" event after add all listerners', () => {
-      e.listenSync('void-event', undefined)
+   await t.step('listen "core/void-event" event after add all listerners', () => {
+      e.listenSync('core/void-event', undefined)
       assertEquals<[number, Listener][] | null>(
-         e.lsListeners('void-event'),
+         e.lsListeners('core/void-event'),
          [
             [0, f404],
             [9, f404],
@@ -85,28 +85,20 @@ Deno.test('[class] PriorityEvent', async (t) => {
             [100, f404],
             [9999, f404],
          ],
-         'expect the listener list to get reordering by priority from 0 → n',
+         'expect the listener list to get sorted by priority from 0 → n',
       )
    })
 
-   await t.step('listen "filter-event" event to receive new value', () => {
-      e.addListener('filter-event', (n) => `#${n.toString(16).toUpperCase()}`)
-      const hex = e.listenSync('filter-event', 0x77DD77)!
+   await t.step('listen "core/filter-event" event to receive new value', () => {
+      e.addListener('core/filter-event', (n) => `#${n.toString(16).toUpperCase()}`)
+      const hex = e.listenSync('core/filter-event', 0x77DD77)!
       assertEquals(hex as unknown as string, '#77DD77')
    })
 })
 
 Deno.test('[function] Listener', async (t) => {
-   const e = PriorityEvent.init()
-
    await t.step('add anonymous arrow syntax', () => {
-      const expect = {
-         name: '',
-         priority: 8,
-         emitter: e,
-         event: 'test-listener',
-      }
-      e.addListener('test-listener', () => assertEquals(this, expect), 8)
+      e.addListener('core/test-listener', () => assertEquals(this, undefined), 8)
    })
 
    await t.step('add anonymous tranditional syntax', () => {
@@ -114,9 +106,9 @@ Deno.test('[function] Listener', async (t) => {
          name: '',
          priority: 9,
          emitter: e,
-         event: 'test-listener',
+         event: 'core/test-listener',
       }
-      e.addListener('test-listener', function () {
+      e.addListener('core/test-listener', function () {
          assertEquals(this, expect)
       }, 9)
    })
@@ -126,10 +118,31 @@ Deno.test('[function] Listener', async (t) => {
          name: 'test',
          priority: 10,
          emitter: e,
-         event: 'test-listener',
+         event: 'core/test-listener',
       }
-      e.addListener('test-listener', function test() {
+      e.addListener('core/test-listener', function test() {
          assertEquals(this, expect)
       })
+   })
+
+   await t.step('checking the input argument', () => {
+      const expect = {
+         name: 'test',
+         priority: 0,
+         emitter: e,
+         event: 'core/test-listener',
+      }
+
+      e.addListener('core/test-listener', function test(arg) {
+         assertEquals(this, expect)
+         assert(
+            typeof arg === 'number',
+            'The argument from listen() function is 0xFFF, expect number type.',
+         )
+         assertEquals(arg, 4095)
+         assertEquals(arg.toString(16).toLowerCase(), 'fff')
+      }, 0)
+
+      e.listen('core/test-listener', 0xFFF)
    })
 })
